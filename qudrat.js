de8c8d74cc4a6ca9c91a9bb2af4dcc7c80034434
WebDeveloper99 <92726540+WebDeveloper99@users.noreply.github.com>
2021-11-09 23:35:28 +0500

    Add files via upload

---
+++ qudrat.js
@@ -0,0 +1,17 @@
+
+
+
+// const myfunc =(a,b,c)=>{
+
+//     var k = 0;
+//     while (a >= c || b >=c) {
+//         a -=c;
+//         b -=c;
+//         k++;
+//     }
+
+//     console.log(k*2);
+
+// }
+
+// myfunc(10,2,1)
